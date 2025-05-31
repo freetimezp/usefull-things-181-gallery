ce359e487403b0234fc09957b3c554ce4f230bec
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-06-01 00:19:57 +0300

    03. usefull-things-181-gallery. GSAP animation.

---
+++ src/assets/js/items.js
@@ -0,0 +1,24 @@
+const items = [
+    "Elemental Truth",
+    "Way of Body",
+    "Predator's Courage",
+    "Ravaged Sword",
+    "Swollen Game",
+    "Green Servant",
+    "Elves of Search",
+    "Nothing's Man",
+    "Wanton Hunter",
+    "Silky Light",
+    "Magic of Captive",
+    "Snow's Elves",
+    "Children of the Angels",
+    "Devoted Sliver",
+    "Dwindling Storms",
+    "Force of School",
+    "Snake's Beginning",
+    "Delicious Person",
+    "Some Magic",
+    "Hidden Secrets",
+];
+
+export default items;
